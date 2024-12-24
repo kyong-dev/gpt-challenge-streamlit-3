3a19a728bcb317d4158071cb11f74c15c8ef5eaf
kyong-dev <kyong.dev@gmail.com>
2024-12-25 02:52:43 +0900

    merge

--- app.py
+++ app.py
@@ -148,7 +148,6 @@ def load_website(url):
     return vector_store.as_retriever()
 
 
-openai_api_key = None
 if os.getenv("OPENAI_API_KEY") is not None and os.getenv("OPENAI_API_KEY") != "":
     openai_api_key = os.getenv("OPENAI_API_KEY")
 
@@ -160,34 +159,31 @@ with st.sidebar:
 
     st.write("<a href='https://github.com/kyong-dev/gpt-challenge-streamlit-3'>https://github.com/kyong-dev/gpt-challenge-streamlit-3</a>", unsafe_allow_html=True)
     url = st.text_input("URL", value="https://developers.cloudflare.com/sitemap-0.xml")
-    if openai_api_key and search_button:
+    if openai_api_key and url:
         os.environ["OPENAI_API_KEY"] = openai_api_key
         if url:
             if ".xml" not in url:
                 with st.sidebar:
                     st.error("Please write down a Sitemap URL.")
+            else:
+                llm = ChatOpenAI(
+                    temperature=0.1,
+                    model="gpt-3.5-turbo",
+                    streaming=True,
+                    api_key=os.environ["OPENAI_API_KEY"],
+                )
+                retriever = load_website(url)
+                query = st.text_input("Ask a question to the website.")
+                if query:
+                    chain = (
+                        {
+                            "docs": retriever,
+                            "question": RunnablePassthrough(),
+                        }
+                        | RunnableLambda(get_answers)
+                        | RunnableLambda(choose_answer)
+                    )
+                    result = chain.invoke(query)
+                    st.markdown(result.content.replace("$", "dollars"))
     else:
         st.error("Please write down your OpenAI key.")
-
-if os.environ["OPENAI_API_KEY"]:
-    llm = ChatOpenAI(
-        temperature=0.1,
-        model="gpt-4o",
-        streaming=True,
-        api_key=os.environ["OPENAI_API_KEY"],
-    )
-
-if os.environ["OPENAI_API_KEY"] and url:
-    retriever = load_website(url)
-    query = st.text_input("Ask a question to the website.")
-    if query:
-        chain = (
-            {
-                "docs": retriever,
-                "question": RunnablePassthrough(),
-            }
-            | RunnableLambda(get_answers)
-            | RunnableLambda(choose_answer)
-        )
-        result = chain.invoke(query)
-        st.markdown(result.content.replace("$", "dollars"))
